0b687a56f8eb7960373faad8af28e71885d73161
andrescabana86 <andrescabana86@gmail.com>
2017-10-26 19:22:17 -0300

    feat: add comment stylish

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 const Comment = ({comment}) => (
 	<section className="comment">
 		<header className="comment-header">
 			<h3 className="comment-author">{comment.author}</h3>
-			<small className="comment-date">{comment.timestamp}</small>
+			<small className="comment-date">
+				<Moment format='MMM DD YYYY HH:MM'>{comment.timestamp}</Moment>
+			</small>
 		</header>
 		<article className="comment-body">
 			<p>{comment.body}</p>
