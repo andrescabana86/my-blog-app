4bde85d9a0582641ba270fdc4bef8011d13b3441
andrescabana86 <andrescabana86@gmail.com>
2017-10-29 14:10:02 -0300

    fix: remove redux debugger

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 // import components
 import App from './components/App';
@@ -11,10 +11,12 @@ import './stylesheets/index.css';
 // reducers
 import reducers from './reducers/';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// import { createStore, applyMiddleware, compose } from 'redux';
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
-    composeEnhancers(applyMiddleware(thunk))
+    // composeEnhancers(applyMiddleware(thunk))
+    applyMiddleware(thunk)
 );
 
 ReactDOM.render(
